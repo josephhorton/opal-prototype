9461bf4191b7f979efb33ec9459dcbc4e5c5de03
josephhorton <joeehorton@protonmail.com>
2024-03-07 15:20:17 +0000

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,5 +4,11 @@
 //
 
 window.GOVUKPrototypeKit.documentReady(() => {
-  // Add JavaScript here
+  new MOJFrontend.ButtonMenu({
+    container: document.querySelector(".moj-button-menu"),
+    mq: "(min-width: 200em)",
+    buttonText: "Menu",
+    buttonClasses:
+      "govuk-button--secondary moj-button-menu__toggle-button--secondary",
+  });
 })
