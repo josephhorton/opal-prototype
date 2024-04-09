8ae1e7d7fdd8e8e3afb04c3f24f5093cf62d2107
josephhorton <joeehorton@protonmail.com>
2024-04-09 15:27:59 +0100

    get ready

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,11 +4,11 @@
 //
 
 window.GOVUKPrototypeKit.documentReady(() => {
-  new MOJFrontend.ButtonMenu({
-    container: document.querySelector(".moj-button-menu"),
-    mq: "(min-width: 200em)",
-    buttonText: "Menu",
-    buttonClasses:
-      "govuk-button--secondary moj-button-menu__toggle-button--secondary",
-  });
+  // new MOJFrontend.ButtonMenu({
+  //   container: document.querySelector(".moj-button-menu"),
+  //   mq: "(min-width: 200em)",
+  //   buttonText: "Menu",
+  //   buttonClasses:
+  //     "govuk-button--secondary moj-button-menu__toggle-button--secondary",
+  // });
 })
