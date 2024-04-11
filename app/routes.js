475c4e47c6d11a256a28a60dc791ccf170a31f2b
josephhorton <joeehorton@protonmail.com>
2024-04-11 09:07:46 +0100

    iteration 2 complete

--- app/routes.js
+++ app/routes.js
@@ -13,9 +13,25 @@ require('./routes/account')(router)
 router.post('/employer-question', function(request, response) {
 
     var employer = request.session.data['employer']
+    var defendant = request.session.data['defendantType']
     if (employer == "employer-yes"){
         response.redirect("./create-account/defendant-employer-details")
-    } else {
+    } 
+    else if ( (employer == "employer-no") && (defendant == "defendant-individual-parent-pay") ){
         response.redirect("./create-account/parent-details")
+    }
+    else {
+        response.redirect("./create-account/end")
+    }
+})
+
+router.post('/employer-details-question', function(request, response) {
+
+    var defendant = request.session.data['defendantType']
+
+    if (defendant == "defendant-individual-parent-pay"){
+        response.redirect("./create-account/parent-details")
+    } else {
+        response.redirect("./create-account/end")
     }
 })
