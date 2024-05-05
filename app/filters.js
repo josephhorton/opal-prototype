f4031be284653cfde8f106b8aca7750b895ffdbf
josephhorton <joeehorton@protonmail.com>
2024-05-05 12:01:33 +0100

    Nearly complete for testing

--- app/filters.js
+++ app/filters.js
@@ -8,3 +8,21 @@ const addFilter = govukPrototypeKit.views.addFilter
 
 // Add your filters here
 
+addFilter('removeEmpty', function (items) {
+    // Directly return undefined if items is falsy (null, undefined, false, 0, '', etc.)
+    if (!items) return
+  
+    // Handle the case when items is a string
+    if (typeof items === 'string') {
+      return items.trim() ? items : undefined
+    }
+  
+    // Handle the case when items is an array
+    if (Array.isArray(items)) {
+      const output = items.filter(item => item)
+      return output.length > 0 ? output : undefined
+    }
+  
+    // Return undefined for other types
+    return
+  })
